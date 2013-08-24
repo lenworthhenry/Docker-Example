3cc83b54159b49abbf6133787e20d685c3e86869
Lenworth Henry <lhenry@lawnovo.com>
2013-08-24 10:52:57 -0400

    Added several files to support yeoman 1.0.  Shoudl be albe to launch server now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,7 @@ var mountFolder = function (connect, dir) {
 module.exports = function (grunt) {
   // load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+grunt.loadNpmTasks('grunt-contrib-compass');
 
   // configurable paths
   var yeomanConfig = {
